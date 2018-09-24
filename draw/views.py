1ca8c6063e04e8b27a1bbadca146b6b67556a323
sung <mailtosungkwon@gmail.com>
2018-09-24 22:10:01 +0100

    before changing the functions model working well

--- draw/views.py
+++ draw/views.py
@@ -7,6 +7,8 @@ from .models import Card
 from .forms import CardForm
 from django.utils import timezone
 
+# for json data
+import requests
 
 def index(request):
     # return HttpResponse("Hello, world. You're at the draw index.")
@@ -16,8 +18,13 @@ def index(request):
 
 
 def card_new(request):
-    # form = CardForm()
-    # return render(request, 'draw/index.html', {'form': form})
+
+    # def get_context_data(self, **kwargs):
+    #     # Call the base implementation first to get a context
+    #     context = super().get_context_data(**kwargs)
+    #     # Add in a QuerySet of all the books
+    #     context['book_list'] = Book.objects.all()
+    #     return context
 
     if request.method == "POST":
         form = CardForm(request.POST)
